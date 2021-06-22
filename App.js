2237e82b63d8d858fc19ef1cb7d98aed664960ce
Rohan <rohanrajput16.phy@gmail.com>
2021-06-23 00:54:59 +0530

    Making folders

--- App.js
+++ App.js
@@ -1,32 +1,16 @@
 import React from 'react'
-import { AppRegistry, View, Text, StyleSheet, TouchableOpacity, Linking, Alert } from 'react-native'
-import { RNCamera } from 'react-native-camera'
-import QRCodeScanner from 'react-native-qrcode-scanner'
-RNCamera
+import { View, Text, StyleSheet } from 'react-native'
+
+import QrCodeScreen from "./app/screens/QrCodeScreen";
+
 export default function App() {
-  const onSucess = (e) => {
-    Linking.openURL(e.data).catch(err => {
-      Alert.alert("Invalid code ", e.data)
-    })
-  }
   return (
-    <View>
-      <Text>QR Code Scanner</Text>
-      <QRCodeScanner
-        containerStyle={{ backgroundColor: "#fff", marginTop: "35%" }}
-        onRead={onSucess}
-        permissionDialogMessage="Need Permisson to access Camera"
-        reactivate={true}
-        reactivateTimeout={10}
-        showMarker={true}
-        markerStyle={{ borderColor: "#fff", borderRadius: 10 }}
-        topContent={
-          <TouchableOpacity>
-            <Text style={{ color: "#fff", fontSize: 20 }}>OK. Got it!</Text>
-          </TouchableOpacity>
-        }
-      />
+    <View style={styles.container}>
+      <QrCodeScreen />
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  container: {}
+})
---
+++ app/screens/QrCodeScreen.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { AppRegistry, View, Text, StyleSheet, TouchableOpacity, Linking, Alert } from 'react-native'
+import { RNCamera } from 'react-native-camera'
+import QRCodeScanner from 'react-native-qrcode-scanner'
+RNCamera
+export default function App() {
+    const onSucess = (e) => {
+        Linking.openURL(e.data).catch(err => {
+            Alert.alert("Invalid code ", e.data)
+        })
+    }
+    return (
+        <View>
+            <Text>QR Code Scanner</Text>
+            <QRCodeScanner
+                containerStyle={{ backgroundColor: "#fff", marginTop: "35%" }}
+                onRead={onSucess}
+                permissionDialogMessage="Need Permisson to access Camera"
+                reactivate={true}
+                reactivateTimeout={10}
+                showMarker={true}
+                markerStyle={{ borderColor: "#fff", borderRadius: 10 }}
+                topContent={
+                    <TouchableOpacity>
+                        <Text style={{ color: "#fff", fontSize: 20 }}>OK. Got it!</Text>
+                    </TouchableOpacity>
+                }
+            />
+        </View>
+    )
+}
+
